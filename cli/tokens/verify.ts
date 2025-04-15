ecff900699a2aff5a604ab5cf04def544b026764
Josh Cunningham <josh@joshcanhelp.com>
2025-04-15 12:57:29 -0700

    feat: default error handing for token verification

--- cli/tokens/verify.ts
+++ cli/tokens/verify.ts
@@ -21,25 +21,12 @@ const {
 
 export const run = async (rawJwt: string) => {
   if (!rawJwt) {
-    Logger().error("Missing JWT in command args");
-    Deno.exit(1);
+    throw new Error("Missing JWT in command args");
   }
 
-  let JWKS;
-  try {
-    JWKS = createRemoteJWKSet(new URL(jwksUrl));
-  } catch (error) {
-    Logger().error(`Could not get JWKS: ${(error as Error).message}`);
-    Deno.exit(1);
-  }
-
-  try {
-    const { payload } = await jwtVerify(rawJwt, JWKS, { issuer });
-    Logger().object(payload);
-  } catch (error) {
-    Logger().error(`Error verifying JWT: ${(error as Error).message}`);
-    Deno.exit(1);
-  }
+  const JWKS = createRemoteJWKSet(new URL(jwksUrl));
+  const { payload } = await jwtVerify(rawJwt, JWKS, { issuer });
+  Logger().object(payload);
 
   if (!introspect || !clientId || !clientSecret) {
     Deno.exit();
@@ -50,23 +37,16 @@ export const run = async (rawJwt: string) => {
   introspectData.append("client_secret", clientSecret);
   introspectData.append("token", rawJwt);
 
-  let tokenActive = false;
-  try {
-    const introspectResponse = await fetch(introspectUrl, {
-      body: introspectData,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept": "application/jwt",
-      },
-    });
-    tokenActive = (await introspectResponse.json()).active;
-  } catch (error) {
-    Logger().error(`Error introspecting JWT: ${(error as Error).message}`);
-    Deno.exit(1);
-  }
+  const introspectResponse = await fetch(introspectUrl, {
+    body: introspectData,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/jwt",
+    },
+  });
 
-  if (tokenActive) {
+  if ((await introspectResponse.json()).active) {
     Logger().success("Token is active");
   } else {
     Logger().error("Token is inactive");
