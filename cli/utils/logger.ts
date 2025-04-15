0c741a93664438bd800a19ffbddac6438ca0fe69
Josh Cunningham <josh@joshcanhelp.com>
2025-04-15 07:47:39 -0700

    feat: logger

---
+++ cli/utils/logger.ts
@@ -0,0 +1,25 @@
+import { getConfig } from "./config.ts";
+
+const {DEBUG_MODE} = getConfig()
+
+export const Logger = () => {
+  return {
+    log: (output: string) => {
+      console.log(output);
+    },
+    object: (output: unknown) => {
+      console.log(output);
+    },
+    success: (output: string) => {
+      console.log("%c✅  " + output, "color: green");
+    },
+    error: (output: string) => {
+      console.log("%c❌  " + output, "color: red");
+    },
+    debug: (output: string) => {
+      if (DEBUG_MODE) {
+        console.log("%c" + output, "color: light-blue");
+      }
+    },
+  };
+};
